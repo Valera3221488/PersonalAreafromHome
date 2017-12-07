22d0741491a8e7413679391a034daeb4f3499b96
beselmonster <magorik5@gmail.com>
2017-12-08 01:50:57 +0200

    git add

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,17 @@
+<header class="header clearfix">
+<nav>
+    <ul class="nav nav-pills float-right">
+
+        <li class="nav-item">
+            <a class="nav-link" href="#">Как это работает</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Связь с Менеджером</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+    </ul>
+</nav>
+    <h3 class="text-muted">Личный Кабинет</h3>
+</header>
